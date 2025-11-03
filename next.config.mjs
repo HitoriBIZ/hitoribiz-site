822783a845ed5c20c21a1548a2361c002047e909
HitoriBIZ <shumatts@gmail.com>
2025-11-03 12:16:04 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, experimental: { typedRoutes: true } };
+const nextConfig = { 
+  reactStrictMode: true, 
+  experimental: { typedRoutes: true },
+  output: 'export' // ←★ この1行を追加！
+};
+
 export default nextConfig;
